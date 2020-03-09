22ff3667fcb6e154760c42f49ab29e0c939afc99
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2020-03-09 19:01:02 +0100

    Fixed bug with control replication
    Fixed a bug not correctly replicating controls on pressing 'add input' button, plus tidied up code

--- libraries/SimpleVocabPlus/Controller/Plugin/Autosuggest.php
+++ libraries/SimpleVocabPlus/Controller/Plugin/Autosuggest.php
@@ -14,63 +14,72 @@
 class SimpleVocabPlus_Controller_Plugin_Autosuggest extends Zend_Controller_Plugin_Abstract
 {
 	/**
+	 * Include all routes (route + controller + actions) that render an
+	 * element form, including actions requested via AJAX.
+	 * 
+	 * @var array
+	 */
+	protected $_defaultRoutes = array(
+		array(
+			'module' => 'default',
+			'controller' => 'items',
+			'actions' => array('add', 'edit', 'change-type')
+		),
+		array(
+			'module' => 'default',
+			'controller' => 'elements',
+			'actions' => array('element-form')
+		)
+	);
+
+	/**
 	 * Cached vocab terms.
 	 */
 	protected $_svpTerms;
 	protected $_elementText;
 
-    /**
-     * Add autosuggest only during defined routes.
-     */
-    public function preDispatch(Zend_Controller_Request_Abstract $request)
-    {
-        $db = get_db();
+	/**
+	 * Add autosuggest only during defined routes.
+	 */
+	public function preDispatch(Zend_Controller_Request_Abstract $request)
+	{
+		$db = get_db();
 
-        // Set NULL modules to default. Some routes do not have a default
-        // module, which resolves to NULL.
-        $module = $request->getModuleName();
-        if (is_null($module)) {
-            $module = 'default';
-        }
-        $controller = $request->getControllerName();
-        $action = $request->getActionName();
-
-        // Include all routes (route + controller + actions) that render an
-        // element form, including actions requested via AJAX.
-        $routes = array(
-            array(
-                'module' => 'default',
-                'controller' => 'items',
-                'actions' => array('add', 'edit', 'element-form', 'change-type'),
-            ),
-        );
+		// Set NULL modules to default. Some routes do not have a default
+		// module, which resolves to NULL.
+		$module = $request->getModuleName();
+		if (is_null($module)) {
+			$module = 'default';
+		}
+		$controller = $request->getControllerName();
+		$action = $request->getActionName();
 
 		$filterFiles = get_option('simple_vocab_plus_files');
 		if ($filterFiles) {
 			// Add the file add/edit route if configured to.
-			$routes[] = array(
+			$this->_defaultRoutes[] = array(
 				'module' => 'default',
 				'controller' => 'files',
 				'actions' => array('add', 'edit')
 			);
 		}
 
-        // Allow plugins to add routes that contain form inputs rendered by
-        // Omeka_View_Helper_ElementForm::_displayFormInput().
-        $routes = apply_filters('svp_suggest_routes', $routes);
+		// Allow plugins to add routes that contain form inputs rendered by
+		// Omeka_View_Helper_ElementForm::_displayFormInput().
+		$routes = apply_filters('svp_suggest_routes', $this->_defaultRoutes);
 
-        // Iterate the defined routes.
-        foreach ($routes as $route) {
-            // Set the autosuggest if the current action matches a defined route.
-            if ($route['module'] === $module
-                    && $route['controller'] === $controller
-                    && in_array($action, $route['actions'])
-                ) {
-                // Iterate the elements that are assigned to a suggest endpoint.
-                $svpAssigns = $db->getTable('SvpAssign')->findAll();
+		// Iterate the defined routes.
+		foreach ($routes as $route) {
+			// Set the autosuggest if the current action matches a defined route.
+			if ($route['module'] === $module
+					&& $route['controller'] === $controller
+					&& in_array($action, $route['actions'])
+				) {
+				// Iterate the elements that are assigned to a suggest endpoint.
+				$svpAssigns = $db->getTable('SvpAssign')->findAll();
 				foreach ($svpAssigns as $svpAssign) {
-                    $element = $db->getTable('Element')->find($svpAssign->element_id);
-                    $elementSet = $db->getTable('ElementSet')->find($element->element_set_id);
+					$element = $db->getTable('Element')->find($svpAssign->element_id);
+					$elementSet = $db->getTable('ElementSet')->find($element->element_set_id);
 					$elementTextTable = $db->getTable('ElementText');
 					$svpTermTable = $db->getTable('SvpTerm');
 
@@ -81,10 +90,10 @@ class SimpleVocabPlus_Controller_Plugin_Autosuggest extends Zend_Controller_Plug
 ?>
 // Add autosuggest to <?php echo $elementSet->name . ':' . $element->name; ?>. Used by the Simple Vocab Plus plugin.
 jQuery(document).bind('omeka:elementformload', function(event) {
-    jQuery('#element-<?php echo $element->id; ?> textarea').autocomplete({
-        minLength: 2,
-        source: <?php echo json_encode($view->url('simple-vocab-plus/endpoint/suggest-proxy/element-id/' . $element->id)); ?>
-    });
+	jQuery('#element-<?php echo $element->id; ?> textarea').autocomplete({
+		minLength: 2,
+		source: <?php echo json_encode($view->url('simple-vocab-plus/endpoint/suggest-proxy/element-id/' . $element->id)); ?>
+	});
 });
 <?php
 						$view->headScript()->captureEnd();
@@ -108,12 +117,16 @@ jQuery(document).bind('omeka:elementformload', function(event) {
 						}
 					}
 
-					add_filter(array('ElementInput', 'Item', $elementSet->name, $element->name),
-								array($this, 'filterElementInput'));
+					add_filter(
+								array('ElementInput', 'Item', $elementSet->name, $element->name),
+								array($this, 'filterElementInput')
+							);
+					// Add the file filter if configured to.
 					if ($filterFiles) {
-						// Add the file filter if configured to.
-						add_filter(array('ElementInput', 'File', $elementSet->name, $element->name),
-									array($this, 'filterElementInput'));
+						add_filter(
+									array('ElementInput', 'File', $elementSet->name, $element->name),
+									array($this, 'filterElementInput')
+								);
 					}
 				}
 			}
@@ -129,10 +142,10 @@ jQuery(document).bind('omeka:elementformload', function(event) {
 	 */
 	public function filterElementInput($components, $args)
 	{
-		// Use the cached vocab terms
 		$hcolor = get_option('simple_vocab_plus_fields_highlight');
 		$hcolor = (preg_match('/#([a-f0-9]{3}){1,2}\b/i', $hcolor) ? 'background-color: ' . $hcolor : '');
 		
+		// Use the cached vocab terms
 		if (empty($this->_svpTerms[$args['element']->id])) {
 			// case autosuggest, values not enforced
 			$components['input'] = get_view()->formTextarea(
