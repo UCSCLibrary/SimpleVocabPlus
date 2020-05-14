1901b81014d3d133d4faa5cc5751f583b9f1c3e2
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2020-05-14 15:13:02 +0200

    Functionality extended to Collections and Exhibits

--- config_form.php
+++ config_form.php
@@ -1,7 +1,10 @@
 <?php
 $simple_vocab_plus_files				= get_option('simple_vocab_plus_files');
+$simple_vocab_plus_collections			= get_option('simple_vocab_plus_collections');
+$simple_vocab_plus_exhibits				= get_option('simple_vocab_plus_exhibits');
 $simple_vocab_plus_fields_highlight		= get_option('simple_vocab_plus_fields_highlight');
 $simple_vocab_plus_fields_description	= get_option('simple_vocab_plus_fields_description');
+$simple_vocab_plus_values_compare		= get_option('simple_vocab_plus_values_compare');
 $view = get_view();
 ?>
 
@@ -19,7 +22,37 @@ $view = get_view();
 	</div>
 </div>
 
-<h2><?php echo __('Warnings'); ?></h2>
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('simple_vocab_plus_collections', __('Apply to Collections')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('If checked, the plugin will be applied to Collections metadata too (by default, this plugin only applies to Items metadata).'); ?>
+		</p>
+		<?php echo $view->formCheckbox('simple_vocab_plus_collections', $simple_vocab_plus_collections, null, array('1', '0')); ?>
+	</div>
+</div>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('simple_vocab_plus_exhibits', __('Apply to Exhibits')); ?>
+	</div>
+	<div class="inputs five columns omega">
+<?php if (plugin_is_active('ExhibitBuilder')): ?>
+		<p class="explanation">
+			<?php echo __('If checked, the plugin will be applied to Exhibits metadata too (by default, this plugin only applies to Items metadata).'); ?>
+		</p>
+		<?php echo $view->formCheckbox('simple_vocab_plus_exhibits', $simple_vocab_plus_exhibits, null, array('1', '0')); ?>
+<?php else: ?>
+		<p class="explanation">
+			<?php echo __('The Exhibit Builder plugin is not installed or active. Install and activate the plugin in order to be able to configure notifications for new Exhibits.'); ?>
+		</p>
+<?php endif ?>
+	</div>
+</div>
+
+<h2><?php echo __('Alerts'); ?></h2>
 
 <div class="field">
 	<div class="two columns alpha">
@@ -29,7 +62,7 @@ $view = get_view();
 		<p class="explanation">
 			<?php echo __('Color hex code (e.g.: #ff0000) to highlight the fields that have autosuggest feature applied (blank means no highlight).'); ?>
 		</p>
-		<?php echo $view->formText('simple_vocab_plus_fields_highlight', $simple_vocab_plus_fields_highlight, null, ''); ?>
+		<?php echo $view->formText('simple_vocab_plus_fields_highlight', $simple_vocab_plus_fields_highlight, array('title'=>__('pound sign (#) followed by six hex values'),'pattern'=>'^#[0-9a-f]{6}$')); ?>
 	</div>
 </div>
 
@@ -42,5 +75,19 @@ $view = get_view();
 			<?php echo __('Adds a notice to the description of fields that have autosuggest feature applied.'); ?>
 		</p>
 		<?php echo $view->formCheckbox('simple_vocab_plus_fields_description', $simple_vocab_plus_fields_description, null, array('1', '0')); ?>
+	</div>
+</div>
+
+<h2><?php echo __('Checks'); ?></h2>
+
+<div class="field">
+	<div class="two columns alpha">
+		<?php echo $view->formLabel('simple_vocab_plus_values_compare', __('Compare element values')); ?>
+	</div>
+	<div class="inputs five columns omega">
+		<p class="explanation">
+			<?php echo __('When examining an element, compare the recorded values with the vocabulary\'s terms.'); ?>
+		</p>
+		<?php echo $view->formCheckbox('simple_vocab_plus_values_compare', $simple_vocab_plus_values_compare, null, array('1', '0')); ?>
 	</div>
 </div>
