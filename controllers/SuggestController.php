8ab33e9ee2b9c30357017504e755a49b0820c942
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-04-09 22:36:06 +0200

    Added source elements's id feature

--- controllers/SuggestController.php
+++ controllers/SuggestController.php
@@ -3,6 +3,7 @@
  * Simple Vocab Plus
  * 
  * @copyright Copyright 2014 UCSC Library Digital Initiatives
+ * @copyright Copyright 2021 Daniele Binaghi
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
 
@@ -24,8 +25,8 @@ class SimpleVocabPlus_SuggestController extends Omeka_Controller_AbstractActionC
 	public function deleteAction() 
 	{
 		$id = $this->getRequest()->getParam('id');
-		$svAssign = $this->_helper->db->getTable('SvpAssign')->find($id);
-		$svAssign->delete();
+		$svpAssign = $this->_helper->db->getTable('SvpAssign')->find($id);
+		$svpAssign->delete();
 		$this->_helper->flashMessenger(__('The element\'s suggest feature was successfully deleted.'), 'success');
 		$this->_helper->redirector('index','index');
 	}
@@ -44,35 +45,48 @@ class SimpleVocabPlus_SuggestController extends Omeka_Controller_AbstractActionC
 	 */
 	public function addAction() {
 
-		$element_id = $this->getRequest()->getParam('element_id');
-		$enforced = $this->getRequest()->getParam('enforced');
-		if ($this->getRequest()->getParam('self-assign')) {
-			$vocab_id = 0;
-			$type = __('self');
-			$enforced = false;
-		} else {
-			$vocab_id = $this->getRequest()->getParam('vocab_id');
-			$type = ($this->_vocabIsLocal($vocab_id) ? __('local') : __('remote'));
+		$element_id = $this->getRequest()->getParam('av_element-id');
+		$enforced = $this->getRequest()->getParam('av_enforced');
+		$sources_id = implode(',', $this->getRequest()->getParam('av_multi-id'));
+		// Do not process too many sources.
+		if (strlen($sources_id) > 100) {
+			$this->_helper->flashMessenger(__('Please select less source elements.'), 'error');
+			$this->_helper->redirector('index','index');
+		}
+
+		switch ($this->getRequest()->getParam('av_source')) {
+			case "self":
+				$vocab_id = 0;
+				$type = __('self');
+				break;
+			case "multi":
+				$vocab_id = 0;
+				$type = __('multi');
+				break;
+			default:
+				$vocab_id = $this->getRequest()->getParam('av_vocab-id');
+				$type = ($this->_vocabIsLocal($vocab_id) ? __('local') : __('remote'));
 		}
 
 		// Do not process empty select options.
 		if ($element_id == '') {
-			$this->_helper->flashMessenger(__('Please select an element to assign.'), 'success');
+			$this->_helper->flashMessenger(__('Please select an element to assign.'), 'error');
 			$this->_helper->redirector('index','index');
 		}
 
 		// Do not process an invalid suggest endpoint.
-		if (!$this->_vocabExists($vocab_id) && $type != 'self') {
+		if ($type != 'self' && (($type == 'multi' && !$sources_id) || ($type == 'vocab' && !$this->_vocabExists($vocab_id)))) {
 			$this->_helper->flashMessenger(__('Invalid suggest endpoint. No changes have been made.'), 'error');
 			$this->_helper->redirector('index','index');
 		}
 		
-		$svAssign = new SvpAssign;
-		$svAssign->element_id = $element_id;
-		$svAssign->type = $type;
-		$svAssign->enforced = $enforced;
-		$svAssign->vocab_id = $vocab_id;
-		$svAssign->save();
+		$svpAssign = new SvpAssign;
+		$svpAssign->element_id = $element_id;
+		$svpAssign->type = $type;
+		$svpAssign->enforced = $enforced;
+		$svpAssign->vocab_id = $vocab_id;
+		$svpAssign->sources_id = $sources_id;
+		$svpAssign->save();
 		$this->_helper->flashMessenger(__('The element\'s suggest feature was successfully added.'), 'success');
 		$this->_helper->redirector('index','index');
 	}
